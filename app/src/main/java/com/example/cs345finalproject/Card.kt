3ba5e3f6c5d78e7df704c4095d7093e59b4b11d3
MattGarling <matthewgarling@gmail.com>
2024-11-14 15:15:10 -0600

    Create Card Class

---
+++ app/src/main/java/com/example/cs345finalproject/Card.kt
@@ -0,0 +1,21 @@
+package com.example.cs345finalproject
+
+internal class Card(var value: String, var type: String) {
+
+    override fun toString(): String {
+        return "$value of $type"
+    }
+
+    fun getValue(): Int {
+        if ("AceJackQueenKing".contains(value)) { // Ace Jack Queen King
+            if (value === "Ace") {
+                return 11
+            }
+            return 10
+        }
+        return value.toInt() // 2-10
+    }
+
+    val isAce: Boolean
+        get() = value === "Ace"
+}
